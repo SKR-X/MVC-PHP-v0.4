431ca91f1295abf3c5f0000c318e7dc0e61871d5
SKR-X <42437146+SKR-X@users.noreply.github.com>
2020-01-29 22:38:54 +0300

    Update Controller.php

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -6,13 +6,31 @@ use App\Core\View as View;
 
 class Controller
 {
-    
     private $View;
-    
-    protected function viewPage($pageName, $config, $queryArr = array())
-    {
-        $this->View = new View();
-        $this->View->viewPage($pageName, $config, $queryArr);
-    }
 
+protected function viewPage($pageName,$config,$queryArr = array())
+{
+    $this->View = new View();
+    $this->View->ViewPage($pageName, $config, $queryArr,$this->checkCookie('lang'));
+}
+
+protected  function viewError($arg)
+{
+    $this->View = new View();
+    $this->View->ViewPage('ErrPage.php', array(), array(), $this->checkCookie('lang'), $arg);
+}
+
+private function checkCookie($name)
+{
+    if(isset($_COOKIE[$name])) 
+    {
+        return $_COOKIE[$name];
+    } else {
+        return false;
+    }
+}
+
+// protected static function ViewPageDB($config  = array()){
+//     View::ViewPageDB($config);
+// }
 }
