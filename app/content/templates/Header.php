ca2cc06884c2707e66df01e7d35f0817e553d32e
SKR-X <42437146+SKR-X@users.noreply.github.com>
2020-02-28 21:05:01 +0300

    Update Header.php

--- app/content/templates/Header.php
+++ app/content/templates/Header.php
@@ -4,7 +4,7 @@
   <title><?= (isset($config['title'])) ? $config['title'] : "Your Company" ?></title>
   <link rel="stylesheet" type="text/css" href=<?= '/app/content/styles/' . $config['css'] . '.css' ?>>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="-" crossorigin="anonymous">
 </head>
 
 <body>
